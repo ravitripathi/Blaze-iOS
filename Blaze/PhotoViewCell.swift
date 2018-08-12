c2457b9b2d34d9d3af5939c7e00dcb087ce3c268
Ravi Tripathi <ravi.tripathi@gdn-commerce.com>
2018-08-13 02:26:12 +0530

    Added new UITableViewCell

---
+++ Blaze/PhotoViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoViewCell.swift
+//  Blaze
+//
+//  Created by Ravi Tripathi on 13/08/18.
+//  Copyright © 2018 Ravi Tripathi. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
